3b3a7404a300ed64d77905f245be35fec928d37c
_lmmmmmm <931420654@qq.com>
2021-12-16 20:59:03 +0800

    [修复](components): 修复访问页面导航到根目录导致的 404

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/React-Ts-Tailwind'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
