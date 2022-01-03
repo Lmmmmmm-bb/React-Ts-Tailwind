17e7e3831248a7830f0cc7ceace49bdc0ba2d345
_lmmmmmm <931420654@qq.com>
2022-01-03 17:57:35 +0800

    [特性](deps): 添加 Semi Design

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,18 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import './index.scss';
 
+import { Button, Toast } from '@douyinfe/semi-ui';
+
 const Main: FC = () => {
-  return <div className='Main text-center text-lg'>Main</div>;
+  useEffect(() => {
+    Toast.success('正在使用 Semi Design');
+  }, []);
+
+  return (
+    <div className='Main text-center text-lg'>
+      <Button>Main</Button>
+    </div>
+  );
 };
 
 export default Main;
