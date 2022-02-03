00e2fcb56a90979ccf81f23703cea0639029e36e
_lmmmmmm <931420654@qq.com>
2022-02-03 16:08:40 +0800

    [特性](deps): 增加 Scss 样式隔离依赖
    增加 typed-scss-modules 依赖对 Scss 样式隔离

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react';
-import './index.scss';
+import styles from './index.module.scss';
 
 const Main: FC = () => {
-  return <div className='Main text-center text-lg'>Main</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.mainText}>Main Text</div>
+    </div>
+  );
 };
 
 export default Main;
